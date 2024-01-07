a4094f46c047bd590720c0f7ccc90b5b9e6a1c03
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-01-07 20:07:34 +0100

    End Of Web Version /07-01-2024/

--- onley js/magic.js
+++ onley js/magic.js
@@ -15,13 +15,14 @@ function exist_and_empty(i,j,n,square) {
 
 
 function magic_square() {
-    let n = parseInt(document.getElementById("size").value);
+    // let n = document.getElementById("size").value;
     // down there we try to see wether the square size is pair or not, if it is pair we add 1 to this size because we cannot generate a magic-square with a pair size
+
+    let n = 4;
+
     if (n%2 ===0) {
         n += 1;
     }
-
-
     // square creat
     var square = new Array(n);
     for (var i = 0; i < square.length; i++) {
@@ -49,18 +50,21 @@ function magic_square() {
             index_j += 1; 
             square[index_i][index_j] = insert_nomber;
             insert_nomber+=1;
+            console.log("1")
         }
         else if (exist_and_empty(index_i-1,(index_j-(n-1)),n,square)==0) {
             index_i -= 1;
             index_j =(index_j-(n-1));
             square[index_i][index_j] = insert_nomber;
             insert_nomber+=1;
+            console.log("2")
         }
         else if (exist_and_empty((index_i+(n-1)),index_j+1,n,square)==0) {
             index_i = n-1;
             index_j +=1 ;
             square[index_i][index_j] = insert_nomber;
             insert_nomber+=1;
+            console.log("3")
         }
         else{
             console.log("error")
@@ -68,40 +72,12 @@ function magic_square() {
             index_j = index_j;
             square[index_i][index_j] = insert_nomber;
             insert_nomber+=1;
+            console.log("4")
         }
+        
+        console.log("k= "+k)
         console.log(square);
     }
 
-
-    // Création d'un tableau HTML
-    var tableau = document.createElement("table");
-    // Appliquer des styles au tableau
-    tableau.style.borderCollapse = "collapse";
-    tableau.style.border = "1px solid #ccc";
-    tableau.style.marginTop = "90px";
-
-
-    // Création des lignes du tableau
-    for (var i = 0; i < n; i++) {
-    var ligne = document.createElement("tr");
-
-    // Création des cellules de la ligne
-    for (var j = 0; j < n; j++) {   
-        var cellule = document.createElement("td");
-        // var texte = document.createTextNode("Ligne " + i + ", Cellule " + j);
-        var texte = document.createTextNode(square[i][j]+ "        ");
-        cellule.appendChild(texte);
-        cellule.style.padding = "10px";
-        cellule.style.backgroundColor = "white";
-        cellule.style.border = "1px solid #ccc";
-        ligne.appendChild(cellule);
-    }
-
-    // Ajout de la ligne au tableau
-    tableau.appendChild(ligne);
-    }
-
-    // Ajout du tableau à un élément existant dans la page (par exemple, un élément avec l'id "conteneur")
-    var conteneur = document.getElementById("magic-square");
-    conteneur.appendChild(tableau);
 }
+magic_square();
